2de87a499a9391403192a5901bbff01d3c3dfa15
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-09-26 10:07:16 -0700

    initial commit, test endpoint

---
+++ src/test/java/com/aravo/library/LibraryAlexandriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravo.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAlexandriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
